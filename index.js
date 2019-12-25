5201f43daabd375c423d2c40f97df94f9930077c
abhishek-chiluka99 <chilukaabhishek9@gmail.com>
2019-12-25 20:35:47 +0530

    updated

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 3000
 app.use(express.static('public'))
 
-app.get('/', (req, res) => res.sendFile(__dirname + '/public/html/hello.html'))
+app.get('/', (req, res) => res.sendFile(__dirname + '/public/resume.html'))
 
 app.get('/data', (req, res) => res.json(obj))
 
